45b5ea7962d83e67b54464666b8f5eceaf6aa26a
fahadu-77 <fahadkallu2002@gmail.com>
2026-01-30 16:59:23 +0530

    deployment changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store/store';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://openlearnhub.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
